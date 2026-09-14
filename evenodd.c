5f9dce774c320d264c4691d42aba7f4ea5465500
Ashutosh38541 <Ashutosh.38541@stu.upes.ac.in>
2026-09-14 13:50:31 +0000

    Add 100-Days-of-code

---
+++ evenodd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+    int num;
+    printf("What is the number? ");
+    scanf("%d", &num);
+    if (num %2 == 0)
+    {
+        printf("%d is even\n", num);
+    }
+    else
+    {
+        printf("%d is odd\n", num);
+    }
+}
